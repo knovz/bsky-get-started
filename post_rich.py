2a574440e8e4698e61f52d679054c6c0982c33b3
KnoVz <h@knovz.com>
2026-01-22 17:01:19 +0100

    post with mention and URI

---
+++ post_rich.py
@@ -0,0 +1,28 @@
+# Not so simple post
+
+from dotenv import load_dotenv
+import os
+from atproto import Client, client_utils
+
+
+load_dotenv()
+
+# bsky cred
+bsky_app_pass = os.getenv('BSKY_APP_PASS')
+bsky_handle = os.getenv('BSKY_HANDLE')
+
+client = Client()
+client.login(bsky_handle, bsky_app_pass)
+
+tb = client_utils.TextBuilder()
+
+tb.text('Hello ')
+tb.mention('atproto.com', 'did:plc:ewvi7nxzyoun6zhxrhs64oiz')
+tb.text(' this is an example of URL sharing ')
+tb.link('CBOR', 'https://en.wikipedia.org/wiki/CBOR')
+tb.text('. You got me to read the article!')
+
+post = client.send_post(tb, langs=['en-US'])
+
+print(post.uri)
+print(post.cid)
