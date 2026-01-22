70af0ec71837d8a6b37faab86876b7a84851ace5
KnoVz <h@knovz.com>
2026-01-22 16:31:43 +0100

    first post

---
+++ post.py
@@ -0,0 +1,17 @@
+from dotenv import load_dotenv
+import os
+from atproto import Client
+
+load_dotenv()
+
+# bsky cred
+bsky_app_pass = os.getenv('BSKY_APP_PASS')
+bsky_handle = os.getenv('BSKY_HANDLE')
+
+client = Client()
+client.login(bsky_handle, bsky_app_pass)
+
+post = client.send_post('Hello world! If you read this, it means I succeeded')
+
+print(post.uri)
+print(post.cid)
